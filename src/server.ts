7a6efeefdb2e0b06b5e92c7f4b16e43ef96133c2
tonovarela <tonovarela@live.com>
2025-01-29 17:58:41 -0600

    .

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine, isMainModule } from '@angular/ssr/node';
 import { render } from '@netlify/angular-runtime/common-engine'
+
+
+
 import express from 'express';
 import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import bootstrap from './main.server';
 
+
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 const indexHtml = join(serverDistFolder, 'index.server.html');
+
 
 const app = express();
 const commonEngine = new CommonEngine();
@@ -42,14 +47,15 @@ app.get(
  */
 app.get('**', (req, res, next) => {
   const { protocol, originalUrl, baseUrl, headers } = req;
-  commonEngine
-    .render({
+  
+  render(commonEngine.render({
       bootstrap,
       documentFilePath: indexHtml,
       url: `${protocol}://${headers.host}${originalUrl}`,
       publicPath: browserDistFolder,
       providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
     })
+  )
     .then((html) => res.send(html))
     .catch((err) => next(err));
 });
