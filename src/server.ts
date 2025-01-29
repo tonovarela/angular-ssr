654ae92b7b606bccf9d87389b72dc66b2cc226df
tonovarela <tonovarela@live.com>
2025-01-29 17:37:01 -0600

    se modifico el server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine, isMainModule } from '@angular/ssr/node';
+import { render } from '@netlify/angular-runtime/common-engine'
 import express from 'express';
 import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -11,6 +12,10 @@ const indexHtml = join(serverDistFolder, 'index.server.html');
 
 const app = express();
 const commonEngine = new CommonEngine();
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  return await render(commonEngine)
+}
 
 /**
  * Example Express Rest API endpoints can be defined here.
