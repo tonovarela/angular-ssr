0e71c4ff3b7db5f9e8bcc7a60b89b8798aec350b
tonovarela <tonovarela@live.com>
2025-01-29 19:40:37 -0600

    Como estaba

--- src/server.ts
+++ src/server.ts
@@ -39,30 +39,21 @@ app.get(
  */
 
 
-export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
-  // Example API endpoints can be defined here.
-  // Uncomment and define endpoints as necessary.
-  // const pathname = new URL(request.url).pathname;
-  // if (pathname === '/api/hello') {
-  //   return Response.json({ message: 'Hello from the API' });
-  // }
-
-  return await render(commonEngine)
-}
 
 
-  // app.get('**', (req, res, next) => {
-  //   const { protocol, originalUrl, baseUrl, headers } = req;
-  //   commonEngine.render({
-  //     bootstrap,
-  //     documentFilePath: indexHtml,
-  //     url: `${protocol}://${headers.host}${originalUrl}`,
-  //     publicPath: browserDistFolder,
-  //     providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-  //   })
-  //     .then((html) => res.send(html))
-  //     .catch((err) => next(err));
-  // });
+
+  app.get('**', (req, res, next) => {
+    const { protocol, originalUrl, baseUrl, headers } = req;
+    commonEngine.render({
+      bootstrap,
+      documentFilePath: indexHtml,
+      url: `${protocol}://${headers.host}${originalUrl}`,
+      publicPath: browserDistFolder,
+      providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+    })
+      .then((html) => res.send(html))
+      .catch((err) => next(err));
+  });
 
 
 
