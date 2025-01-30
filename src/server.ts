e8e0a7b25bb4d7095fab40f2cd1f9e867ff17557
tonovarela <tonovarela@live.com>
2025-01-29 20:44:53 -0600

    Listo para usar en netlify

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,7 @@ import { render } from '@netlify/angular-runtime/common-engine'
 const commonEngine = new CommonEngine()
 
 export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
-  // Example API endpoints can be defined here.
-  // Uncomment and define endpoints as necessary.
-  // const pathname = new URL(request.url).pathname;
-  // if (pathname === '/api/hello') {
-  //   return Response.json({ message: 'Hello from the API' });
-  // }
+
 
   return await render(commonEngine)
 }
