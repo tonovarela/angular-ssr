9f39d9d5d712904c03233f1aa9c57fdb4ed0efa6
tonovarela <tonovarela@live.com>
2025-01-29 18:20:13 -0600

    server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine, isMainModule } from '@angular/ssr/node';
 import { render } from '@netlify/angular-runtime/common-engine'
-
-
-
 import express from 'express';
 import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -47,15 +44,13 @@ app.get(
  */
 app.get('**', (req, res, next) => {
   const { protocol, originalUrl, baseUrl, headers } = req;
-  
   render(commonEngine.render({
-      bootstrap,
-      documentFilePath: indexHtml,
-      url: `${protocol}://${headers.host}${originalUrl}`,
-      publicPath: browserDistFolder,
-      providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-    })
-  )
+    bootstrap,
+    documentFilePath: indexHtml,
+    url: `${protocol}://${headers.host}${originalUrl}`,
+    publicPath: browserDistFolder,
+    providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+  }))
     .then((html) => res.send(html))
     .catch((err) => next(err));
 });
@@ -70,5 +65,8 @@ if (isMainModule(import.meta.url)) {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
+
+
+
 
 
