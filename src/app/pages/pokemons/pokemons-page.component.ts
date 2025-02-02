b2e973ad592c14c656bbc497fd3a3188134abf7e
tonovarela <tonovarela@live.com>
2025-02-01 20:30:11 -0600

    Añadir componente de esqueleto para la lista de pokemons y mejorar la estructura de la página de pokemons

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,12 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from '../../pokemons/components/pokemon-list/pokemon-list.component';
+import { PokemonListSkeletonComponent } from './ui/pokemon-list-skeleton/pokemon-list-skeleton.component';
 
 
 @Component({
   selector: 'pokemons-page',
-  imports: [PokemonListComponent],
+  imports: [PokemonListComponent,PokemonListSkeletonComponent],
   templateUrl: './pokemons-page.component.html',
   styleUrl: './pokemons-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonsPageComponent { }
+export default class PokemonsPageComponent 
+//implements OnInit,OnDestroy
+ {
+  
+  // public isLoading =signal(true);
+  // private appRef= inject(ApplicationRef);
+  // private $appState = this.appRef.isStable.subscribe((isStable) => {
+  //   console.log('App state changed', isStable);
+  // });
+
+
+  // ngOnInit(): void {  
+  //   setTimeout(() => {
+  //     this.isLoading.set(false);
+  //   },1000);
+  // } 
+
+  // ngOnDestroy(): void {
+  //   this.$appState.unsubscribe();
+  // }
+}
--- src/app/pokemons/components/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemons/components/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
+import { PokemonListSkeletonComponent } from '../../../pages/pokemons/ui/pokemon-list-skeleton/pokemon-list-skeleton.component';
 
 @Component({
   selector: 'pokemon-list',
